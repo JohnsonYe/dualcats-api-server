89a0f8badff29bdea961f6d12bba86e8fe7e3703
johnson <johnson.ye@locbit.com>
2020-08-29 21:59:29 -0700

    change ssl path

--- app.js
+++ app.js
@@ -45,28 +45,25 @@ app.get('*', function(req, res){
     res.status(404).send('Welcome to my 404 🏠');
 });
 
+const PORT = process.env.PORT || 8084;
 if (process.env.SSL_ENABLE != "true") {
-    http.createServer(app).listen(process.env.DEFAULT_PORT || 8084, () => {
-        console.log('Server is running on %s mode, listening at port %s', process.env.NODE_ENV , process.env.DEFAULT_PORT || 8084);
+    http.createServer(app).listen(process.env.PORT || 8084, () => {
+        console.log('Server is running on %s mode, listening at port %s', process.env.NODE_ENV , PORT || 8084);
     });
 } else {
-    const PORT = (process.env.SSL_ENABLE == "true") ? process.env.SSL_PORT : process.env.DEFAULT_PORT;
     const serverOptions = {
         httpsServerOptions: {
             secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1,
-            ca: [fs.readFileSync('./ssl/digiCertCA.crt')],
-            key: fs.readFileSync('./ssl/server.key'),
-            cert: fs.readFileSync('./ssl/server.crt'),
-            ciphers : ["ECDHE-RSA-AES256-GCM-SHA384","ECDHE-RSA-AES128-GCM-SHA256","ECDH-RSA-AES128-SHA256","DHE-RSA-AES128-SHA256","HIGH","!AES128-GCM-SHA256","!AES128-SHA","!ECDHE-RSA-AES256-SHA384","!ECDHE-RSA-AES256-SHA","!AES256-GCM-SHA384", "!AES256-SHA256", "!AES256-SHA", "!ECDHE-RSA-AES128-SHA", "!TLS_RSA_WITH_AES_128_GCM_SHA256", "!AES128-SHA256", "!TLS_RSA_WITH_AES_128_CBC_SHA", "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA" ].join(':'),
+            key: fs.readFileSync('../ssl/server.key'),
+            cert: fs.readFileSync('../ssl/server.crt'),
+            ciphers : ["ECDHE-RSA-AES256-GCM-SHA384","ECDHE-RSA-AES128-GCM-SHA256","ECDH-RSA-AES128-SHA256","DHE-RSA-AES128-SHA256","HIGH","!AES128-GCM-SHA256","!AES128-SHA","!ECDHE-RSA-AES256-SHA384","!ECDHE-RSA-AES256-SHA","!AES256-GCM-SHA384", "!AES256-SHA256", "!AES256-SHA", "!ECDHE-RSA-AES128-SHA", "!TLS_RSA_WITH_AES_128_GCM_SHA256", "!AES128-SHA256", "!TLS_RSA_WITH_AES_128_CBC_SHA", "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!SRP", "!CAMELLIA"].join(':'),
             honorCipherOrder: true
         }
     };
     const https_server = https.createServer(serverOptions, app);
     https_server.listen(PORT, () => {
-        console.log('Server is running on %s mode, listening at port %s', process.env.NODE_ENV , process.env.DEFAULT_PORT);
+        console.log('Server is running on %s mode, listening at port %s', process.env.NODE_ENV , PORT);
     });
 }
-
-
 
 module.exports.app = app;
